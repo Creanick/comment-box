bbc2fd041304bb496f25d2d731110992844e72e9
manick <manickware@gmail.com>
2019-09-19 14:11:02 +0530

    Updates

---
+++ precache-manifest.06a5608c48e4b7c99edd830b1bee4517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403ad8db96be2d5c95cb2bdc41f6dbcc",
+    "url": "/comment-box/index.html"
+  },
+  {
+    "revision": "2a660914afce007d3def",
+    "url": "/comment-box/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "c1d9bf5a91cb4f3bfc9b",
+    "url": "/comment-box/static/js/2.6282a724.chunk.js"
+  },
+  {
+    "revision": "2a660914afce007d3def",
+    "url": "/comment-box/static/js/main.d0745a7b.chunk.js"
+  },
+  {
+    "revision": "888680b7e17e1bf4dfa8",
+    "url": "/comment-box/static/js/runtime~main.1f848a13.js"
+  }
+]);
