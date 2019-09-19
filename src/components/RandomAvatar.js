1fa24e8493bbabc6e06261f4942a49791a7dd72b
manick <manickware@gmail.com>
2019-09-19 11:40:25 +0530

    add random style generator

--- src/components/RandomAvatar.js
+++ src/components/RandomAvatar.js
@@ -8,115 +8,121 @@ const randomArrayPicker = array => {
   }
   return array[getRandomInt(0, array.length - 1)];
 };
+
+const topTypes = [
+  "ShortHairDreads01",
+  "ShortHairShaggyMullet",
+  "ShortHairShortCurly",
+  "ShortHairShortFlat",
+  "ShortHairShortRound",
+  "ShortHairShortWaved",
+  "ShortHairTheCaesar"
+];
+const accessoriesTypes = [
+  "Kurt",
+  "Prescription02",
+  "Prescription01",
+  "Round",
+  "Sunglasses",
+  "Wayfarers"
+];
+const hairColors = [
+  "Auburn",
+  "Black",
+  "Blonde",
+  "BlondeGolden",
+  "Brown",
+  "BrownDark",
+  "PastePink",
+  "Platinum",
+  "Red",
+  "SilverGray"
+];
+const facialHairs = [
+  "Blank",
+  "BeardMedium",
+  "BeardLight",
+  "BeardMagestic",
+  "MoustacheFancy",
+  "MoustacheMagnum"
+];
+const facialHairColors = [
+  "Auburn",
+  "Black",
+  "Blonde",
+  "BlondeGolden",
+  "Brown",
+  "BrownDark",
+  "Platinum",
+  "Red"
+];
+const clothes = [
+  "BlazerShirt",
+  "BlazerSweater",
+  "CollarSweater",
+  "Hoodie",
+  "ShirtCrewNeck"
+];
+const colorFabrics = [
+  "Black",
+  "Blue01",
+  "Blue02",
+  "Blue03",
+  "Gray01",
+  "Gray02",
+  "Heather",
+  "PastelBlue",
+  "PastelGreen",
+  "PastelOrange",
+  "PastelRed",
+  "PastelYellow",
+  "Red",
+  "White"
+];
+const eyes = ["Default", "Happy", "Side", "Squint", "Surprised"];
+const eyeBrows = [
+  "Default",
+  "DefaultNatural",
+  "FlatNatural",
+  "RaisedExcited",
+  "RaisedExcitedNatural",
+  "UpDown",
+  "UpDownNatural"
+];
+const mouths = ["Default", "Smile", "Twinkle"];
+const skins = [
+  "Tanned",
+  "Yellow",
+  "Pale",
+  "Light",
+  "Brown",
+  "DarkBrown",
+  "Black"
+];
+export const randomStyleGenerator = () => {
+  return {
+    topType: randomArrayPicker(topTypes),
+    accessoriesType: randomArrayPicker(accessoriesTypes),
+    hairColor: randomArrayPicker(hairColors),
+    facialHairType: randomArrayPicker(facialHairs),
+    facialHairColor: randomArrayPicker(facialHairColors),
+    clotheType: randomArrayPicker(clothes),
+    clotheColor: randomArrayPicker(colorFabrics),
+    eyeType: randomArrayPicker(eyes),
+    eyebrowType: randomArrayPicker(eyeBrows),
+    mouthType: randomArrayPicker(mouths),
+    skinColor: randomArrayPicker(skins)
+  };
+};
+const staticStyle = randomStyleGenerator();
 function RandomAvatar(props) {
-  const { size = 100 } = props;
-  const topTypes = [
-    "ShortHairDreads01",
-    "ShortHairShaggyMullet",
-    "ShortHairShortCurly",
-    "ShortHairShortFlat",
-    "ShortHairShortRound",
-    "ShortHairShortWaved",
-    "ShortHairTheCaesar"
-  ];
-  const accessoriesTypes = [
-    "Kurt",
-    "Prescription02",
-    "Prescription01",
-    "Round",
-    "Sunglasses",
-    "Wayfarers"
-  ];
-  const hairColors = [
-    "Auburn",
-    "Black",
-    "Blonde",
-    "BlondeGolden",
-    "Brown",
-    "BrownDark",
-    "PastePink",
-    "Platinum",
-    "Red",
-    "SilverGray"
-  ];
-  const facialHairs = [
-    "Blank",
-    "BeardMedium",
-    "BeardLight",
-    "BeardMagestic",
-    "MoustacheFancy",
-    "MoustacheMagnum"
-  ];
-  const facialHairColors = [
-    "Auburn",
-    "Black",
-    "Blonde",
-    "BlondeGolden",
-    "Brown",
-    "BrownDark",
-    "Platinum",
-    "Red"
-  ];
-  const clothes = [
-    "BlazerShirt",
-    "BlazerSweater",
-    "CollarSweater",
-    "Hoodie",
-    "ShirtCrewNeck"
-  ];
-  const colorFabrics = [
-    "Black",
-    "Blue01",
-    "Blue02",
-    "Blue03",
-    "Gray01",
-    "Gray02",
-    "Heather",
-    "PastelBlue",
-    "PastelGreen",
-    "PastelOrange",
-    "PastelRed",
-    "PastelYellow",
-    "Red",
-    "White"
-  ];
-  const eyes = ["Default", "Happy", "Side", "Squint", "Surprised"];
-  const eyeBrows = [
-    "Default",
-    "DefaultNatural",
-    "FlatNatural",
-    "RaisedExcited",
-    "RaisedExcitedNatural",
-    "UpDown",
-    "UpDownNatural"
-  ];
-  const mouths = ["Default", "Smile", "Twinkle"];
-  const skins = [
-    "Tanned",
-    "Yellow",
-    "Pale",
-    "Light",
-    "Brown",
-    "DarkBrown",
-    "Black"
-  ];
-  const [change, setChange] = useState(Math.random());
+  const { size = 100, onClick = () => {}, style = staticStyle } = props;
   return (
-    <span onClick={() => setChange(Math.random())}>
+    <span onClick={onClick}>
       <Avatar
         style={{ width: size + "px", height: size + "px" }}
         avatarStyle="Circle"
-        topType={randomArrayPicker(topTypes)}
-        accessoriesType={randomArrayPicker(accessoriesTypes)}
-        hairColor={randomArrayPicker(hairColors)}
-        facialHairType={randomArrayPicker(facialHairs)}
-        facialHairColor={randomArrayPicker(facialHairColors)}
-        clotheType={randomArrayPicker(clothes)}
-        clotheColor={randomArrayPicker(colorFabrics)}
-        eyeType={randomArrayPicker(eyes)}
-        eyebrowType={randomArrayPicker(eyeBrows)}
-        mouthType={randomArrayPicker(mouths)}
-        skinColor={randomArrayPicker(skins)}
+        {...style}
       />
     </span>
   );
