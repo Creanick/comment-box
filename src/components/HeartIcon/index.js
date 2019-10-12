9877dbec7027a0487b3103e4829b3a068ab95852
creanick <manickware@gmail.com>
2019-10-12 08:50:05 +0530

    solid prop added

--- src/components/HeartIcon/index.js
+++ src/components/HeartIcon/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classes from "./HeartIcon.module.css";
-function HeartIcon({ width }) {
+function HeartIcon({ solid, width, ...rest }) {
   return (
     <svg
-      className={classes.heartIcon}
+      className={classes.heartIcon + " global-transition"}
       aria-hidden="true"
       focusable="false"
       data-prefix="fas"
@@ -12,10 +13,12 @@ function HeartIcon({ width }) {
       width={width || 25}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
+      {...rest}
     >
       <path
         fill="red"
-        className={classes.solid}
+        opacity={solid ? 1 : 0}
+        className={classes.solid + " global-transition"}
         d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"
       ></path>
       <path
@@ -26,5 +29,8 @@ function HeartIcon({ width }) {
     </svg>
   );
 }
-
+HeartIcon.propTypes = {
+  solid: PropTypes.bool,
+  width: PropTypes.number
+};
 export default HeartIcon;
