2da0a647b5fd058c53ebeafda06751232f0a8cc3
creanick <manickware@gmail.com>
2019-10-17 11:03:00 +0530

    add post compoent dynamic story

--- src/views/Post/Post.stories.js
+++ src/views/Post/Post.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
+import { text, boolean, date, number } from "@storybook/addon-knobs";
 import Post from ".";
 export default {
   title: "Post|Main Post"
@@ -18,3 +19,19 @@ const postOptions = {
 export const normal = () => (
   <Post {...postOptions}>My name is manick lal jamadar</Post>
 );
+
+export const dynamic = () => {
+  const dynamicOptions = {
+    ...postOptions,
+    name: text("name", postOptions.name),
+    date: date("Date"),
+    liked: boolean("Is Liked? ", false),
+    likes: number("Likes", 234),
+    comments: number("comments", 4567)
+  };
+  return (
+    <Post {...dynamicOptions}>
+      {text("Content", "My name is manick lal jamadar")}
+    </Post>
+  );
+};
